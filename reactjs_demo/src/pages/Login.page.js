22623e00efa87fcfeca9fceb4b1353dbe3548052
hungbkvn9x1@gmail.com <hungbkvn9x1@gmail.com>
2023-05-31 11:54:49 +0700

    up

--- reactjs_demo/src/pages/Login.page.js
+++ reactjs_demo/src/pages/Login.page.js
@@ -1,16 +1,19 @@
 import React, { useState, useContext } from "react";
 import { login } from "../services/auth.service";
 import UserContext from "../store/context.store";
+import api from "../services/api";
 
-const Login = () =>{
-    const [ email,setEmail ] = useState()
-    const {state, dispatch} = useContext(UserContext);
-    const [ password,setPassword ] = useState()
-    const lg = async (event) =>{
+const Login = (props) => {
+    const [email, setEmail] = useState()
+    const { state, dispatch } = useContext(UserContext);
+    const [password, setPassword] = useState()
+    const lg = async (event) => {
         event.preventDefault();
-        const user = await login(email,password)
-        localStorage.setItem("state",JSON.stringify(state));
-        dispatch({type: "LOGIN", payload: user})
+        const user = await login(email, password)
+        localStorage.setItem("state", JSON.stringify(state));
+        dispatch({ type: "LOGIN", payload: user })
+        api.defaults.headers.common["Authorization"] = `Bearer ${user.token}`
+        console.log(user)
     }
 
     const inputHandleEmail = (event) => {
@@ -21,27 +24,27 @@ const Login = () =>{
         const password = event.target.value
         setPassword(password)
     }
-    return(
+    return (
         <>
-         <div className="container">
-           <div className="row">
-               <div className="col-3">
-                <form onSubmit={lg}>
-                    <div>
-                    <label>Email</label>
-                    <input type="text" onChange={inputHandleEmail} name="email" placeholder="email..."/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <form onSubmit={lg}>
+                            <div>
+                                <label>Email</label>
+                                <input type="text" onChange={inputHandleEmail} name="email" placeholder="email..." />
+                            </div>
+                            <div>
+                                <label>Password</label>
+                                <input type="password" onChange={inputHandlePassword} name="password" placeholder="password..." />
+                            </div>
+                            <div>
+                                <button type="submit" className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                    <label>Password</label>
-                    <input type="password" onChange={inputHandlePassword} name="password" placeholder="password..."/>
-                    </div>
-                    <div>
-                        <button type="submit" className="btn btn-primary">Login</button>
-                    </div>
-                    </form>
-               </div>
+                </div>
             </div>
-        </div>
         </>
     )
 }
